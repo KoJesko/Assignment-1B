e71bb1941716ee5a039f65ae3cc66e1655899332
Ari Kotler <jansherremway@gmail.com>
2024-05-30 19:16:38 -0400

    Initial commit

---
+++ Assignment 1B.py
@@ -0,0 +1,18 @@
+print(3 * 3 * 3)  # This will print the result of 3 cubed, which is 27
+
+print(4 + (5 / 5))  # This will print the result of 4 plus the result of 5 divided by 5, which is 5
+
+print(8 + (2 * 3))  # This will print the result of 8 plus the result of 2 times 3, which is 14
+
+print(0 - 0.0)  # This will print the result of 0 minus 0.0, which is 0.0
+
+print(5.5 * 10.3)  # This will print the result of 5.5 times 10.3, which is 56.65
+
+# The ** operator is used for exponentiation. For example, 2 ** 3 will give 8 because it's 2 raised to the power of 3.
+print(2 ** 3)
+
+# The % operator is used for getting the remainder of a division. For example, 10 % 3 will give 1 because the remainder of 10 divided by 3 is 1.
+print(10 % 3)
+
+# You can combine these operators with others. For example, (5 ** 2) % 3 will give 1 because 5 squared is 25 and the remainder of 25 divided by 3 is 1.
+print((5 ** 2) % 3)
